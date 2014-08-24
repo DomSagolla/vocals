400bea1e496b34aa522c4dcf425487e347c8443b
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-08-23 19:53:26 -0700

    changed routing

--- app.rb
+++ app.rb
@@ -14,25 +14,22 @@ get '/' do
 
   Twilio::TwiML::Response.new do |response|
     response.Say "Hello #{contactsName}. Record your message."
-    response.Record :maxLength => '5', :trim => "trim-silence", :playBeep => "true", :action => '/recording', :method => 'get'
+    response.Record :maxLength => '5', :trim => "trim-silence", :playBeep => "true", :action => '/playback', :method => 'get'
 
   end.text
 
 end
 
 
-get '/recording' do
-  account_sid = ENV['ACCOUNT_SID']
-  auth_token = ENV['AUTH_TOKEN']
-  @client = Twilio::REST::Client.new account_sid, auth_token
+get '/playback' do
 
   Twilio::TwiML::Response.new do |response|
     response.Play params['RecordingUrl']
     response.Say 'Here are the recordings from today.'
-    latestTenRecordings = @client.account.recordings.list()[1..9]
+    latestTenRecordings = client().account.recordings.list()[1..3]
 
     latestTenRecordings.each do |recording|
-      
+
       response.Play recording.mp3()
 
     end
@@ -40,4 +37,16 @@ get '/recording' do
     response.Say 'Goodbye.'
 
   end.text
+end
+
+
+helpers do
+
+  def client
+    account_sid = ENV['ACCOUNT_SID']
+    auth_token = ENV['AUTH_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+
 end
